ce04c16f5756bb8a2357be5b0d5bbe189f232ff3
Florian Braun <flo@myprivatemail.de>
2016-10-21 22:57:30 +0200

    FileIO UnitTests

--- vorlesung/projekt/lenaflomarco/src/UserManager/FileDatabase_test.go
+++ vorlesung/projekt/lenaflomarco/src/UserManager/FileDatabase_test.go
@@ -1,16 +1,69 @@
 package UserManager
 
+import (
+	"testing"
+	"os"
+	"github.com/stretchr/testify/assert"
+	"github.com/pkg/errors"
+)
 
-import "testing"
-import "strings"
-import "github.com/stretchr/testify/assert"
+const TESTFILEPATH = "testdir/tmp.db"
+const TESTFOLDER = "testdir"
 
-const TESTFILEPATH  = ""
+var TestData = []UserRecord{
+	{UID:1, Email:"flo@myprivatemail.de", Name:"Florian Braun", HashedPassword:"???", Salt:"???"},
+	{UID:1, Email:"lena.hoinkis@gmail.com", Name:"Lena Hoinkis", HashedPassword:"???", Salt:"???"},
+}
 
-var TestData string = ""
+type UserRecord struct {
+	UID            int64  //Unique ID
+	Email          string //Email
+	Name           string //Name
+	HashedPassword string //Password Hashed and Salted
+	Salt           string //Salt
+}
 
-func saveToFile(t *testing.T) {
+func test_FileSave(t *testing.T) {
 
-	saveToFile(TestData,)
+	response, err := saveToFile(TestData, TESTFILEPATH)
+	if err != nil {
+		t.Error(response)
+	}
 
+	if !exists(TESTFOLDER) || !exists(TESTFILEPATH) {
+		t.Error("Testfolder or Testfile was not created automatically")
+	}
+
+}
+
+func test_FileRead(t *testing.T) {
+
+	data, err := readFromFile(TESTFILEPATH)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t,TestData,data,"Saved and Read data is not Equal")
+
+}
+
+// test_finaly = Aufräumen nach dem Test
+func test_finaly(t *testing.T){
+	cleanup(TESTFOLDER)
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, errors.Wrap(err,"Some error while checking if file exists")
+}
+
+func cleanup(path string) error {
+	err := os.Remove(path)
+	return err
 }
