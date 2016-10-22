864d2d5ed25830c1f64951a6bb4b2f68d3f3fcf0
Florian Braun <flo@myprivatemail.de>
2016-10-22 14:05:46 +0200

    added session and timestamp to db

--- vorlesung/projekt/lenaflomarco/src/UserManager/UserManagerData.go
+++ vorlesung/projekt/lenaflomarco/src/UserManager/UserManagerData.go
@@ -1,6 +1,9 @@
 package UserManager
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 
 //The Data which is accessed by Go should be loaded in RAM(Hashmap for best reading performance) and only synced to disk if a change is imminent/done.
@@ -19,6 +22,8 @@ type UserRecord struct {
 	Name           string //Name
 	HashedPassword string //Password Hashed and Salted
 	Salt           string //Salt
+	Session        string //SessionCookie
+	SessionLast    time.Time   //Timestamp when last Interaction took place -> for session timeout
 }
 
 //Actual Global acessible Variable for saving the user Data
