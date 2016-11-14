998355387695cdc5d8d70b00806d1de8a09c6acc
Florian Braun <flo@myprivatemail.de>
2016-11-14 14:29:30 +0100

    Typo

--- vorlesung/projekt/lenaflomarco/src/UserManager/UserManager_test.go
+++ vorlesung/projekt/lenaflomarco/src/UserManager/UserManager_test.go
@@ -25,7 +25,8 @@ func TestMakeSalt(t *testing.T) {
 //TestVerifyHash
 func TestVerifyHash(t *testing.T) {
 	var v bool
-	v = VerifyHash("Psw", "ABC", "25928498b28c3268d911dd78d7ff820e0f14ed32b7ac2d397746f1778038b968d9e6364fd4b3da2e7026bdf574c104779fac9ce9064b6b9ae09ac043f8d131d4")
+	v = VerifyHash("Psw", "ABC",
+		"25928498b28c3268d911dd78d7ff820e0f14ed32b7ac2d397746f1778038b968d9e6364fd4b3da2e7026bdf574c104779fac9ce9064b6b9ae09ac043f8d131d4")
 	if !v {
 		t.Error("Expected true , got ", v)
 	}
