c1c1a4b59d8289f1b4ede53f4f422a493d7e769b
Florian Braun <flo@myprivatemail.de>
2016-11-16 18:18:18 +0100

    Basic HTTPS Server
    Most Secure HTTPS Impl. Possible

--- vorlesung/projekt/lenaflomarco/src/Server.go
+++ vorlesung/projekt/lenaflomarco/src/Server.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
+	"net/http"
 	"Utils"
-	"Flags"
 )
 
 func init() {
@@ -10,11 +11,35 @@ func init() {
 }
 
 func main() {
+	requestMultiplexer := http.NewServeMux()
 
-	Utils.LogError("Test")
-	Utils.LogWarning("Test")
-	Utils.LogInfo("Test")
-	Utils.LogDebug(Flags.GetWorkDir())
+	requestMultiplexer.HandleFunc("/", root)
 
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      requestMultiplexer,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
+	Utils.HandlePanic(srv.ListenAndServeTLS("res/certificates/server.pem", "res/certificates/server.key"))
 }
 
+func root(w http.ResponseWriter, req *http.Request)  {
+	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+	w.Write([]byte("TEST TEST TEST \n"))
+}
