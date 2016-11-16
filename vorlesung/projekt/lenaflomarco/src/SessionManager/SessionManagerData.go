fa4422d6c30b95cff93f54db4102beec21e6a297
Florian Braun <flo@myprivatemail.de>
2016-11-16 17:00:42 +0100

    SessionManager uses Flag system + Doc

--- vorlesung/projekt/lenaflomarco/src/SessionManager/SessionManagerData.go
+++ vorlesung/projekt/lenaflomarco/src/SessionManager/SessionManagerData.go
@@ -3,34 +3,46 @@ package SessionManager
 import (
 	"sync"
 	"time"
+	"flag"
 )
 
 
-// SETTINGS
+// SESSION_KEY_LENGTH Length of the SessionKey which gets used after Authentication for access control.
 const (
-	SESSION_KEY_LENGTH = 200
-	SESSION_TIMEOUT_IN_SECODS = 900 // 900s = 15min
+	SESSION_KEY_LENGTH = 64
 )
 
-//SessionMap
+// sessionTimeout is a Variable wich holds the timeout in Second and can be set by Flag e.g -sessionTimeout=900
+var sessionTimeout = flag.Int("sessionTimeout", 900, "Time in Seconds after which a inactive Session gets invalid")
 
-type SessionMap map[string] SessionRecord
+//SessionMap arranges SessionRecords in a Map for faster Access by SessionID
+type SessionMap map[string]SessionRecord
 
-//RWmutex for concurrent access and prevention of Mutual Exclusion,
-// the mutex should be unlocked if data got written to permanent storage
+//SessionStorage RWmutex for concurrent access and prevention of Mutual Exclusion. Holds A SessionMap and adds Mutex functionality
 type SessionStorage struct {
 	SessionMap
 	sync.RWMutex
 }
 
-//Entry of a single user
+//SessionRecord Entry of a single user.
+//Email is unique and identifies a user.
+//Session is the sessionKey which is used for further authentication.
+//SessionLast is a timestamp of the last access to the SessionRecord, this is used to implement a timeout functionality.
 type SessionRecord struct {
-	UID            int16  //Unique ID
-	Email          string //Email
-	Session        string //SessionCookie
-	SessionLast    time.Time   //Timestamp when last Interaction took place -> for session timeout
+	Email       string    //Email
+	Session     string    //SessionCookie
+	SessionLast time.Time //Timestamp when last Interaction took place -> for session timeout
 }
 
-//Actual Global acessible Variable for saving the user Data
+//Actual acessible Variable for saving the user Data
 var managersSessionStorage SessionStorage
 
+//Init managersSessionStorage and parse Flags
+func init() {
+	managersSessionStorage.Lock()
+	defer managersSessionStorage.Unlock()
+
+	//Init Map
+	managersSessionStorage.SessionMap = make(SessionMap)
+
+}
