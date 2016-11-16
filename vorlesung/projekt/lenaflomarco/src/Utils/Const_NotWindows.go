d5d0c9326ab9ae2df15aaf7deb3632b360520802
Florian Braun <flo@myprivatemail.de>
2016-11-16 15:19:57 +0100

    Cleanup

---
+++ vorlesung/projekt/lenaflomarco/src/Utils/Const_NotWindows.go
@@ -0,0 +1,14 @@
+// +build !windows
+
+package Utils
+
+// Colors for Terminal , only on Unix systems
+const (
+	ANSI_COLOR_RED = "\x1b[31m"
+	ANSI_COLOR_GREEN = "\x1b[32m"
+	ANSI_COLOR_YELLOW = "\x1b[33m"
+	ANSI_COLOR_BLUE = "\x1b[34m"
+	ANSI_COLOR_MAGENTA = "\x1b[35m"
+	ANSI_COLOR_CYAN = "\x1b[36m"
+	ANSI_COLOR_RESET = "\x1b[0m"
+)
---
+++ vorlesung/projekt/lenaflomarco/src/Utils/Const_Windows.go
@@ -0,0 +1,14 @@
+// +build windows
+
+package Utils
+
+// Colors for Unix, set to empty for Windows because windows cant :O
+const (
+	ANSI_COLOR_RED = ""
+	ANSI_COLOR_GREEN = ""
+	ANSI_COLOR_YELLOW = ""
+	ANSI_COLOR_BLUE = ""
+	ANSI_COLOR_MAGENTA = ""
+	ANSI_COLOR_CYAN = ""
+	ANSI_COLOR_RESET = ""
+)
