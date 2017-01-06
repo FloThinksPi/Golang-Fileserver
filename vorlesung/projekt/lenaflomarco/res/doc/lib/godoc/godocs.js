b9c8960b3f90c36fec51bb9e83f2e30eb86f82f6
Florian Braun <flo@myprivatemail.de>
2017-01-06 23:59:33 +0100

    Fix Doc

--- vorlesung/projekt/lenaflomarco/res/doc/lib/godoc/godocs.js
+++ vorlesung/projekt/lenaflomarco/res/doc/lib/godoc/godocs.js
@@ -273,6 +273,8 @@ function personalizeInstallInstructions() {
   }
   if (os != 'windows') {
     $('#windowsInstructions').hide();
+    $('.testUnix').show();
+    $('.testWindows').hide();
   } else {
     if (ext != 'msi') {
       $('#windowsInstallerInstructions').hide();
@@ -280,6 +282,8 @@ function personalizeInstallInstructions() {
     if (ext != 'zip') {
       $('#windowsZipInstructions').hide();
     }
+    $('.testUnix').hide();
+    $('.testWindows').show();
   }
 
   var download = "https://storage.googleapis.com/golang/" + filename;
@@ -291,6 +295,14 @@ function personalizeInstallInstructions() {
   message.insertAfter('#nav');
 
   window.location = download;
+}
+
+function updateVersionTags() {
+  var v = window.goVersion;
+  if (/^go[0-9.]+$/.test(v)) {
+    $(".versionTag").empty().text(v);
+    $(".whereTag").hide();
+  }
 }
 
 $(document).ready(function() {
@@ -309,6 +321,7 @@ $(document).ready(function() {
   setupCallgraphs();
   toggleHash();
   personalizeInstallInstructions();
+  updateVersionTags();
 
   // godoc.html defines window.initFuncs in the <head> tag, and root.html and
   // codewalk.js push their on-page-ready functions to the list.
